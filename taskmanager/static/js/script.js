813f3eef09bfb0f6dcc0bfe501d8d88fc8c34ae6
SCollins <sje.collins@gmail.com>
2023-04-18 11:58:18 +0000

    Task CRUD halfway

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, { 
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+    var collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
+});
